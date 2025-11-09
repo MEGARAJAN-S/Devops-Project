05e1fcf4a8c220ca212d11cf5eeb858783a6d466
MEGARAJAN-S <megarajanmegan@gmail.com>
2025-11-09 19:01:43 +0530

    Initial commit - Hello World Project By Megarajan

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! From JAVA CI/CD Pipeline" );
+        System.out.println("Megarajan S IOT A 22011102054");
+    }
+}
